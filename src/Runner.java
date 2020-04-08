293c90dce617f2234339cca451668e7674d517cd
David <temiloluwaw@gmail.com>
2020-04-08 17:08:53 +0100

    commit test

---
+++ src/Runner.java
@@ -0,0 +1,9 @@
+
+public class Runner {
+
+	public static void main(String[] args) {
+		hello hello = new hello();
+		System.out.println(hello.yes("David"));
+	}
+
+}
---
+++ src/hello.java
@@ -0,0 +1,8 @@
+
+public class hello {
+public String yes(String yy) {
+	return yy;
+}
+	
+	
+}
